bba040ca5df72d364c1645f5e2d88cd8d4976f3e
MOHAMED Button Games <59149047+MOHAMED-Button-Games@users.noreply.github.com>
2022-02-22 05:07:40 +0300

    Add files via upload

--- script_404.js
+++ script_404.js
@@ -4,25 +4,29 @@ if (navigator.userAgent.indexOf("Chrome") != -1)
 {
 	if (!document.getElementById(ID))
 	{
-		var head   = document.getElementsByTagName('head')[0];
-		var link   = document.createElement('link');
+		var head   = document.getElementsByTagName('head')[0]
+		var link   = document.createElement('link')
 		link.id    = ID;
-		link.rel   = "stylesheet";
-		link.type  = "text/css";
-		link.href  = "style.css";
-		head.appendChild(link);
+		link.rel   = "stylesheet"
+		link.type  = "text/css"
+		link.href  = "style.css"
+		head.appendChild(link)
 	}
 }
 else
 {
 	if (!document.getElementById(ID))
 	{
-		var head   = document.getElementsByTagName('head')[0];
-		var link   = document.createElement('link');
+		var head   = document.getElementsByTagName('head')[0]
+		var link   = document.createElement('link')
 		link.id    = ID;
-		link.rel   = "stylesheet";
-		link.type  = "text/css";
-		link.href  = "Undefined/style_Undefined.css";
-		head.appendChild(link);
+		link.rel   = "stylesheet"
+		link.type  = "text/css"
+		link.href  = "Undefined/style_Undefined.css"
+		head.appendChild(link)
 	}
+	document.getElementById("0").href = "index_Undefined"
+	document.getElementById("1").href = "Games_Undefined"
+	document.getElementById("2").href = "Videos_Undefined"
+	document.getElementById("3").href = "About_Undefined"
 }
